0f703a569ddbb8c1c3009d0d1f095ca96f994bdf
Michael Smith <m.t.smith@sheffield.ac.uk>
2023-02-15 12:44:48 +0000

    work with n-dimensions

--- advectionGPdatasets/__init__.py
+++ advectionGPdatasets/__init__.py
@@ -4,7 +4,12 @@ def proposeboundary(X):
     """
     Proposes a boundary ([tmin,xmin,ymin],[tmax,xmax,ymax]) given an X
     """
-    minX,maxX = np.min(X[:,[0,2,3]],0),np.max(X[:,[1,2,3]],0)
+    axesA = list(range(X.shape[1]))
+    axesB = axesA.copy()
+    del axesA[1]
+    del axesB[0]
+    minX,maxX = np.min(X[:,axesA],0),np.max(X[:,axesB],0)
+    #minX,maxX = np.min(X[:,[0,2,3]],0),np.max(X[:,[1,2,3]],0)
     edge = (maxX-minX)*0.1 #10% edge
     boundary=(list(np.floor(minX-edge)),list(np.ceil(maxX+edge)))
     return boundary
