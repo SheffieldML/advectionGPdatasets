f64991230d26055265a4f41b6881dd86bff30a52
Mike Smith <lionfishy@gmail.com>
2023-11-07 12:55:42 +0000

    scripts for server

--- juypter/paper_estimating_flow3d_plot.py
+++ juypter/paper_estimating_flow3d_plot.py
@@ -1,42 +1,124 @@
-        
-#Generate figure
-rhms = []
+from time import sleep
+import pickle
+from advectionGPdatasets.roundhill import RoundHill,RoundHillModel
+from advectionGP.kernels import EQ
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+parser = argparse.ArgumentParser(description='Run roundhill pollution model')
+parser.add_argument("--features", help="Number of features", type=int)
+parser.add_argument("--particles", help="Number of particles per observation", type=int)
+parser.add_argument("--ls", help="Lengthscale", type=float)
+parser.add_argument("--k0", help="k0", type=float)
+parser.add_argument("--resT", help="resolution (in time)", type=int)
+parser.add_argument('--ground', default=False, action='store_true', help='include reflecting ground plane')
+args = parser.parse_args()
+Nfeat = args.features
+Npart = args.particles
+res = [args.resT,100,100,10]
+ls = args.ls
+s2 = 1000
+k0 = args.k0
+if args.ground:
+    walls = [(3,0,-1)]
+else:
+    walls = None
+#rhms = []
 plt.figure()
-for rep in np.arange(1):
-    fn = getfn(walls,s2,ls,k0,Nfeat,Npart,rep)
-    rhm = pickle.load(open(fn,'rb'))
-    rhms.append(rhm)
+def getfn(walls,s2,ls,k0,Nfeat,Npart,rep=-1,fig=False):
+    if fig:
+        fn = 'roundhillmodel3d_%s_noholdout_s2=%d_ls=%d_k0=%0.1f_Nfeat=%d_Nparticles=%d.pdf' % ('wall' if walls is not None else '',s2,ls,k0,Nfeat,Npart)
+    else:
+        fn = 'roundhillmodel3d_%s_noholdout_s2=%d_ls=%d_k0=%0.1f_Nfeat=%d_Nparticles=%d_rep=%d.pkl' % ('wall' if walls is not None else '',s2,ls,k0,Nfeat,Npart,rep)
+    return fn
+
+#Runs out of memory "OSError: [Errno 14] Bad address" error occurs.
+
+#for rep in range(20):
+#    fn = getfn(walls,s2,ls,k0,Nfeat,Npart,rep)
+#    print("Loading %s: " % fn,end="")
+#    try:
+#        rhm = pickle.load(open(fn,'rb'))
+#        print("Success")
+#    except Exception as err:
+#        print("Failed")
+#        print(err)
+#        continue
+#    rhms.append(rhm)
+#    sleep(3)
+
+#for rhm in rhms:
+data = []
+for rep in range(21):
+    fn = 'small_'+getfn(walls,s2,ls,k0,Nfeat,Npart,rep)
+    print("Loading %s: " % fn,end="")
+    try:
+        #rhm = pickle.load(open(fn,'rb'))
+        a = pickle.load(open(fn,'rb'))
+        sourcesall = a['sourcesall']
+        gridstepsize = a['gridstepsize']
+        boundary = a['boundary']
+
+        print("Success")
+    except Exception as err:
+        print("Failed")
+        print(err)
+        continue
+
     for sample_at_peak,col,style,lab in zip([True,False],['b','g'],['-','--'],['around peak','whole domain']):
-        sourcemasses = []        
+        sourcemasses = []    
         if sample_at_peak:
-            sources = rhm.results['sources']['all'][0,:,(73-15):(73+15),(12-12):(12+20),:3].copy()
+            #sources = rhm.results['sources']['all'][0,:,(73-15):(73+15),(12-12):(12+20),:3].copy()
+            sources = sourcesall[0,:,(73-15):(73+15),(12-12):(12+20),:3].copy()
         else:
-            sources = rhm.results['sources']['all'][0,:,:,:,:].copy()
-        pickle.dump(sources,open('temp_'+lab+'.pkl','wb'))
+            #sources = rhm.results['sources']['all'][0,:,:,:,:].copy()
+            sources = sourcesall[0,:,:,:,:].copy()
+        #pickle.dump(sources,open('temp_'+lab+'.pkl','wb'))
         #sources[sources<0]=0
         #np.sum(sources)*np.prod(rhm.mInfer.getGridStepSize()[0][1:])
-        mass = np.sum(sources,(1,2,3))*np.prod(rhm.mInfer.getGridStepSize()[0][1:])
+        #mass = np.sum(sources,(1,2,3))*np.prod(rhm.mInfer.getGridStepSize()[0][1:])
+        mass = np.sum(sources,(1,2,3))*np.prod(gridstepsize[1:])
 
         sourcemasses.append(mass)
         #plt.plot(times,mass[:-1])
-        times = np.arange(rhm.mInfer.boundary[0][0],rhm.mInfer.boundary[1][0],rhm.mInfer.getGridStepSize()[0][0])[:-1]
+        #times = np.arange(rhm.mInfer.boundary[0][0],rhm.mInfer.boundary[1][0],rhm.mInfer.getGridStepSize()[0][0])[:-1]
+        times = np.arange(boundary[0][0],boundary[1][0],gridstepsize[0])[:-1]
         m = np.mean(sourcemasses,0)[:-1]/1e3 #grams from mg.
-        s = (np.std(sourcemasses,0)/np.sqrt(13))[:-1]/1e3 #13 samples (finding rough SE! argh)
+        #s = (np.std(sourcemasses,0)/np.sqrt(13))[:-1]/1e3 #13 samples (finding rough SE! argh)
         #s = np.sqrt((np.var(sourcemasses,0)/13+single_sample_var))[:-1]/1e3
         #s = np.sqrt(single_sample_var)[:-1]/1e3
         #sourcemasses = np.array(sourcemasses)
-        plt.plot(times,m,style+col,label=lab)
-        plt.fill_between(times,m-s*1.96,m+s*1.96,color=col,alpha=0.2)
+        if len(data)==0: data.append(times)
+        data.append(m)
+        plt.plot(times,m,style+col)#,label=lab if rep==0 else None)
+        #plt.fill_between(times,m-s*1.96,m+s*1.96,color=col,alpha=0.2)
         plt.ylabel('Mass flow rate / $g\;s^{-1}$')
         plt.xlabel('Time / $s$')
         #plt.ylim([-5,12.5])#12.500])
-        plt.xlim(rhm.mInfer.boundary[0][0],rhm.mInfer.boundary[1][0]-4)
-    
-    #plt.title("%0.3f" % k0)
-    #plt.hlines(39.5e3,0,300,'r')
+        #plt.xlim(rhm.mInfer.boundary[0][0],rhm.mInfer.boundary[1][0]-4)
+        plt.xlim(boundary[0][0],boundary[1][0]-4)
+        break #skip whole domain
+#plt.title("%0.3f" % k0)
+#plt.hlines(39.5e3,0,300,'r')
 plt.grid()
 plt.hlines(0,-200,1000)
 plt.legend()
 #plt.ylim([-3,3])
 fn = getfn(walls,s2,ls,k0,Nfeat,Npart,fig=True)
 plt.savefig(fn)
+
+plt.figure()
+data = np.array(data)
+plt.figure()
+mean = np.mean(data[1:,:],0)
+std = np.std(data[1:,:],0)/np.sqrt(len(data)-1)
+plt.plot(data[0,:],mean,color='b')
+plt.fill_between(data[0,:],mean-std*1.96,mean+std*1.96,color='b',alpha=0.2)
+print(len(data))
+plt.xlim(boundary[0][0],boundary[1][0]-4)
+plt.ylim([0,100])
+plt.grid()
+plt.ylabel('Mass flow rate / $g\;s^{-1}$')
+plt.xlabel('Time / $s$')
+
+plt.savefig('b.pdf')
--- juypter/paper_estimating_flow_plot.py
+++ juypter/paper_estimating_flow_plot.py
@@ -4,36 +4,54 @@ from advectionGP.kernels import EQ
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-#parameters 
-
-Nfeat = 2000
-Npart = 30
-k_0 = 0.2
-res = [200,100,100]
-ls = 2.0
-s2 = 1000
-
+parser = argparse.ArgumentParser(description='Run roundhill pollution model')
+parser.add_argument("--features", help="Number of features", type=int)
+parser.add_argument("--particles", help="Number of particles per observation", type=int)
+parser.add_argument("--ls", help="Lengthscale", type=float)
+parser.add_argument("--k0", help="k0", type=float)
+parser.add_argument("--resT", help="resolution (in time)", type=int)
+parser.add_argument('--ground', default=False, action='store_true', help='include reflecting ground plane')
+args = parser.parse_args()
 
 rhms = []
 plt.figure()
-for rep in np.arange(10):
+def getfn(walls,s2,ls,k0,Nfeat,Npart,rep,fig):
+    if fig:
+        fn = 'roundhillmodel3d_%s_noholdout_s2=%d_ls=%d_k0=%0.1f_Nfeat=%d_Nparticles=%d.pdf' % ('wall' if walls is not None else '',s2,ls,k0,Nfeat,Npart)
+    else:
+        fn = 'roundhillmodel3d_%s_noholdout_s2=%d_ls=%d_k0=%0.1f_Nfeat=%d_Nparticles=%d_rep=%d.pkl' % ('wall' if walls is not None else '',s2,ls,k0,Nfeat,Npart,rep)    
+    return fn
+
+for rep in range(20):
+    fn = getfn(walls,s2,ls,k0,Nfeat,Npart,rep)
+    print("Loading %s: " % fn,end="")
     try:
-        rhm = pickle.load(open('roundhillmodel_noholdout_s2=%d_ls=%d_k0=0.5_Nfeat=%d_Nparticles=%d_rep=%d.pkl' % (s2,ls,Nfeat,Npart,rep),'rb'))
+        rhm = pickle.load(open(fn,'rb'))
+        print("Success")
     except:
+        print("Failed")
         continue
     rhms.append(rhm)
+
+for rhm in rhms:
     for sample_at_peak,col,style,lab in zip([True,False],['b','g'],['-','--'],['around peak','whole domain']):
         sourcemasses = []        
         if sample_at_peak:
-            sources = rhm.results['sources']['all'][0,:,35:50,0:20].copy()
+            sources = rhm.results['sources']['all'][0,:,(73-15):(73+15),(12-12):(12+20),:3].copy()
         else:
-            sources = rhm.results['sources']['all'][0,:,:,:].copy()
-        mass = np.sum(sources,(1,2))*np.prod(rhm.mInfer.getGridStepSize()[0][1:])
+            sources = rhm.results['sources']['all'][0,:,:,:,:].copy()
+        pickle.dump(sources,open('temp_'+lab+'.pkl','wb'))
+        #sources[sources<0]=0
+        #np.sum(sources)*np.prod(rhm.mInfer.getGridStepSize()[0][1:])
+        mass = np.sum(sources,(1,2,3))*np.prod(rhm.mInfer.getGridStepSize()[0][1:])
 
         sourcemasses.append(mass)
+        #plt.plot(times,mass[:-1])
         times = np.arange(rhm.mInfer.boundary[0][0],rhm.mInfer.boundary[1][0],rhm.mInfer.getGridStepSize()[0][0])[:-1]
         m = np.mean(sourcemasses,0)[:-1]/1e3 #grams from mg.
         #s = (np.std(sourcemasses,0)/np.sqrt(13))[:-1]/1e3 #13 samples (finding rough SE! argh)
+        #s = np.sqrt((np.var(sourcemasses,0)/13+single_sample_var))[:-1]/1e3
+        #s = np.sqrt(single_sample_var)[:-1]/1e3
         #sourcemasses = np.array(sourcemasses)
         plt.plot(times,m,style+col,label=lab)
         #plt.fill_between(times,m-s*1.96,m+s*1.96,color=col,alpha=0.2)
@@ -41,9 +59,12 @@ for rep in np.arange(10):
         plt.xlabel('Time / $s$')
         #plt.ylim([-5,12.5])#12.500])
         plt.xlim(rhm.mInfer.boundary[0][0],rhm.mInfer.boundary[1][0]-4)
+
+#plt.title("%0.3f" % k0)
+#plt.hlines(39.5e3,0,300,'r')
 plt.grid()
 plt.hlines(0,-200,1000)
 plt.legend()
-plt.ylim([-3,3])
-plt.savefig('estimated_flowrate_s2=%d_ls=%d_k0=0.5_Nfeat=%d_Nparticles=%d_rep=%d.pdf' % (s2,ls,Nfeat,Npart,rep))
- 	
+#plt.ylim([-3,3])
+fn = getfn(walls,s2,ls,k0,Nfeat,Npart,fig=True)
+plt.savefig(fn)
