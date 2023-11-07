8471334e79a32c8f5616f0e183dfe69edfcbc19d
Mike Smith <lionfishy@gmail.com>
2023-11-07 12:55:50 +0000

    scripts for server

--- juypter/submission_paper_estimating_flow.sh
+++ juypter/submission_paper_estimating_flow.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# Request 100Gb of real memory (mem)
-#SBATCH --mem=150G
-#SBATCH --cpus-per-task=10
-
+#SBATCH --mem=220G
+#SBATCH --cpus-per-task=20
+#
 # load the module for the program we want to run
 #module load apps/gcc/foo
+#/users/md1xmtsx/anaconda3/bin/python paper_estimating_flow3d.py --rep=$SLURM_ARRAY_TASK_ID --features=50000 --particles=50 --ls=2 --k0=0.459 --resT 200
+/users/md1xmtsx/anaconda3/bin/python paper_estimating_flow3d.py --rep=$SLURM_ARRAY_TASK_ID --features=100000 --particles=50 --ls=2 --k0=0.459 --resT 200
 
-/users/md1xmtsx/anaconda3/bin/python paper_estimating_flow3d.py
+
+#test (quick!)
+#/users/md1xmtsx/anaconda3/bin/python paper_estimating_flow3d.py --rep=$SLURM_ARRAY_TASK_ID --features=2000 --particles=20 --ls=2 --k0=0.459 --resT 200
